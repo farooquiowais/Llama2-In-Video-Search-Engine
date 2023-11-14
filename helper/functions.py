e7fc1a7d4fdbbfe76387a05a8b568d78e41b9e9c
farooquiowais <owais.datascientist@gmail.com>
2023-11-14 19:28:51 +0000

    fixed some typos

--- helper/functions.py
+++ helper/functions.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer
 from secrets import auth_token
-from constants.secretes import auth_token
+from constants.keys import auth_token
 import streamlit as st
 import torch
 import re
