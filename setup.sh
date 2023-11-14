bfff010d8808a6d840f31e14e876eeb6a3b0e0e0
farooquiowais <owais.datascientist@gmail.com>
2023-11-14 19:32:59 +0000

    added some additional files for the setup

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Any preliminary setup
+echo "Setting up environment..."
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install packages from requirements.txt
+pip install -r requirements.txt
+
+# Install PyTorch, torchvision, and torchaudio for specific CUDA version
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117 --upgrade
+
+echo "Installation completed successfully!"
